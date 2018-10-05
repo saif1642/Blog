6be32e274ab892443d148fca4bbc569a5d3f697e
saifdev1642 <saifdev1642@gmail.com>
2018-10-05 23:20:13 +0600

    Using sumernote for post content textarea

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,7 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/toastr.min.css') }}" rel="stylesheet">
+    @yield('styles')
 </head>
 <body>
     <div id="app">
@@ -132,5 +133,6 @@
            toastr.info("{{ Session::get('info') }}");
         @endif
     </script>
+    @yield('scripts')
 </body>
 </html>
